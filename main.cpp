73e77b394a56b9585c3908f894c306b98651d483
weixin <yanweixin@zoho.com>
2018-12-15 13:53:08 +0800

    add vscode files

--- main.cpp
+++ main.cpp
@@ -1,4 +1,7 @@
 #include <iostream>
+#include <stdio.h>
+#include <typeinfo>
+#include <string>
 
 using std::cin;
 using std::cout;
@@ -6,6 +9,10 @@ using std::cout;
 int main(int argc, char const *argv[])
 {
     cout << "Hello Word" << std::endl;
+    unsigned char i = 0;
+    cout << typeid(i).name() << std::endl;
+    std::string mystr = "string";
+    cout << mystr << '\n';
     cin.get();
     return 0;
 }
